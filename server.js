7ef8c549204abdd100bd4b16c27712ef064a926a
keisa <skycindy89@naver.com>
2023-02-03 13:27:56 +0900

    파파고 api 주석

--- server.js
+++ server.js
@@ -56,14 +56,14 @@ app.get("/keyword", function (req, res) {
 
 const client_id = process.env.papagoID; // 파파고 아이디
 const client_secret = process.env.papagoSecret; // 파파고 키
-const api_url = "https://openapi.naver.com/v1/papago/n2mt";
+const api_url = "https://openapi.naver.com/v1/papago/n2mt";//파파고 n2mt방식 url
 
 app.get("/translate", function (req, res) {
-  let keyword = req.query;
+  let keyword = req.query;//키워드api에서 redirect된 쿼리스트링을 keyword=req.query로 받아서 세팅
 
   var str = "";
-  var json = JSON.stringify(keyword);
-  var data = JSON.parse(json);
+  var json = JSON.stringify(keyword);//제이슨형식 문자열로 변경(stringfy)
+  var data = JSON.parse(json);//제이슨형식으로 변경된것을 자바스크립트의 객체로 변경(parse)
   var json2 = JSON.stringify(data);
   var data2 = JSON.parse(json2);
   var data3 = JSON.parse(data2.translatedText);
@@ -72,16 +72,17 @@ app.get("/translate", function (req, res) {
       str += data3.sentences[i].keywords[j].word + ",";
     }
   }
+  //str에 kewyord를 data3에서 translatedText(쿼리스트링받은거)를 넣어 값에 접근하여 for문으로 배열을 읽어옴
   var replaceAt = function (input, index, character) {
     return (
       input.substr(0, index) +
       character +
       input.substr(index + character.length)
     );
-  };
-  var change_str = str.slice(0, -1);
-  res.cookie("key", change_str, { maxAge: 600000 });
-  const options = {
+  };//for문으로 돌려서 읽어온 값들을 ","기준으로 출력했기에 마지막에도 ","가 붙어있는데 이를 제거하기위하여 함수설정
+  var change_str = str.slice(0, -1);//슬라이스하여 change_str에 삽입
+  res.cookie("key", change_str, { maxAge: 600000 });//쿠키에 이름을 "key"로 설정하고 value에 change_str을 담아서 리스폰스함(마지막화면에서 키워드 한글출력을 위해서)
+  const options = {//번역하기 위한 클라이언트 아이디, 비밀번호 그리고 번역하고자 하는 문장의 form을 담아서 제이슨형식으로 발송
     url: api_url,
     form: { source: "ko", target: "en", text: change_str },
     headers: {
@@ -92,9 +93,9 @@ app.get("/translate", function (req, res) {
   request.post(options, function (error, response, body) {
     //리퀘스트
     if (!error && response.statusCode == 200) {
-      let trans = JSON.parse(body);
-      let translated = trans.message.result.translatedText;
-      res.redirect("loading?translatedText=" + translated);
+      let trans = JSON.parse(body);//위에서 발송한 url을 파싱하여 자바스크립트 객체로 만들고 
+      let translated = trans.message.result.translatedText;//번역된 키워드들이 들어있는 translatedText만 추출하여
+      res.redirect("loading?translatedText=" + translated);// /loading으로 쿼리스트링형태로 발송 하면 133줄의 /loading에서 칼로가 받아 그림을 그리기 시작함
     } else {
       res.status(response.statusCode).end();
       console.log("error = " + response.statusCode);
